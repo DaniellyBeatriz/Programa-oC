173cbebdfbae9ef152fe360d76206faa132c1bd7
DaniellyBeatriz <oliveiradanyy5@gmail.com>
2025-09-27 12:19:42 -0300

    Update modularidade e abstração.c

--- modularidade e abstração.c
+++ modularidade e abstração.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+// Função génerica para entrada de dados
+void entradaDados() {
+  // código para a função entradaDados
+}
+
+// Função génerica para calculo na média
+float calcularMedia() {
+  //código para a função calcularMedia
+}
+
+// Função genérica para exibir dados
+void exibirMedia() {
+  // código para função exibirMedia
+}
+
+int main() {
+
+  entrandoDados();
+  media = calcularMedia();
+  exibirMedia();
+
+  return 0;
+}
+
+
+
+
+
+
 
